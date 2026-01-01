c65e3067b65509759183e989240faafe46884796
Lukasz <lcheciak@gmail.com>
2026-01-01 17:33:22 +0100

    fix tests and workflow

--- tests/test_dummy.py
+++ tests/test_dummy.py
@@ -1,2 +1,19 @@
-def test_simple_check():
-    assert 1 == 1
+from fastapi.testclient import TestClient
+from sentiment_app.app import app
+
+client = TestClient(app)
+
+def test_read_health():
+    response = client.get("/health")
+    assert response.status_code == 200
+    assert response.json() == {"status": "healthy"}
+
+def test_predict_sentiment():
+    payload = {"text": "I really love this lab!"}
+    response = client.post("/predict", json=payload)
+    
+    assert response.status_code == 200
+    data = response.json()
+    assert "sentiment" in data
+    assert "confidence" in data
+    assert isinstance(data["sentiment"], str)
